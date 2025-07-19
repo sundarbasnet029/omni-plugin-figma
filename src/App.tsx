acc2388a550c79446a41d3f7ec1eda9ea2e3f70b
sundarbasnet <sundarbasnet029@gmail.com>
2025-07-19 21:32:50 +0545

    initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+    <h2>Rectangle Creator</h2>
+<p>Count: <input id="count" type="number" value="5"/></p>
+<button id="create">Create</button>
+<button id="cancel">Cancel</button>
+    </>
+  )
+}
+
+export default App
